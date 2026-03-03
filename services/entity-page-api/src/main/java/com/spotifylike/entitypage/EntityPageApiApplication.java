8f89036790bd69a7997201fed08891d9fdb0dcb2
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-03-04 00:44:05 +0100

    Create EntityPageApiApplication.java

---
+++ services/entity-page-api/src/main/java/com/spotifylike/entitypage/EntityPageApiApplication.java
@@ -0,0 +1,12 @@
+package com.spotifylike.entitypage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityPageApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntityPageApiApplication.class, args);
+    }
+}
