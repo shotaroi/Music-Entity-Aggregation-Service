f16b6da685b761cbdc1b46a5727d3f98a5a70df6
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-03-04 00:44:42 +0100

    Create PopularityServiceApplication.java

---
+++ services/popularity-service/src/main/java/com/spotifylike/popularity/PopularityServiceApplication.java
@@ -0,0 +1,12 @@
+package com.spotifylike.popularity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopularityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopularityServiceApplication.class, args);
+    }
+}
